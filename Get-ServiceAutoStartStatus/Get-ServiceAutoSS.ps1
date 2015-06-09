4c10987a921eaf2c8ea58505c9a7b180bb3b47e2
janderson <j.and@outlook.com>
2015-06-09 10:26:09 -0700

    Initial Commit

---
+++ Get-ServiceAutoStartStatus/Get-ServiceAutoSS.ps1
@@ -0,0 +1,68 @@
+<#
+        .DESCRIPTION
+        Gets the 'Automatically Start' Service Status of a Given Computer
+
+        .PARAMETER computername
+        Specifies the computername. Accepts Lists. Defaults to env:COMPUTERNAME
+
+        .EXAMPLE
+        Get-ServiceAutoSS -Computername COMPUTER1,COMPUTER2,COMPUTER3
+		Get-ADComputer -Filter {OperatingSystem -like "Window*Server*"} | Select Name | %{Get-ServiceAutoSS -Computername $_.Name} | ft
+		Get-ADComputer -Filter {OperatingSystem -like "Window*Server*"} | Select Name | %{Get-ServiceAutoSS -Computername $_.Name -VERBOSE} | ft
+
+        .NOTES
+        AUTHOR: James Anderson
+		LAST UPDATED: 6/9/2015
+#>
+Function Get-ServiceAutoSS {
+[cmdletbinding()]
+Param(
+[Parameter(Position=0,ValueFromPipeline=$True)]
+[alias("cn","pscomputername")]
+[string[]]$Computername = $env:COMPUTERNAME
+)
+ 
+Begin {
+    Write-Verbose -Message "Starting $($MyInvocation.Mycommand)"  
+ 
+    $wmiParam=@{
+     class = 'Win32_Service'
+     filter= "StartMode='Auto' AND State='Stopped'"
+     ComputerName= $Null
+     ErrorAction= "SilentlyContinue"
+    }
+	# List of Services to Ignore. 
+	$Ignore=@( 
+		'Microsoft .NET Framework NGEN v4.0.30319_X64', 
+		'Microsoft .NET Framework NGEN v4.0.30319_X86', 
+		'Multimedia Class Scheduler', 
+		'Performance Logs and Alerts', 
+		'SBSD Security Center Service', 
+		'Shell Hardware Detection', 
+		'Software Protection', 
+		'TPM Base Services'; 
+	) 
+
+} #begin
+ 
+Process {
+ 
+foreach ($computer in $computername) {
+     Write-Verbose "Querying $computer.toUpper())"
+     $wmiParam.Computername=$Computer
+    Try {
+     Get-WMIObject @wmiParam | Select @{name="Computername";Expression={$_.SystemName}},
+     Name,DisplayName,StartMode,State,Status | Where-Object {$Ignore -notcontains $_.Displayname}
+    }
+    Catch {
+      Write-Warning "Failed to retrieve WMI information from $computername"
+      Write-Warning $_.Exception.Message
+    }
+ } #foreach
+} #process
+ 
+End {
+    Write-Verbose -Message "Ending $($MyInvocation.Mycommand)"
+} #end
+ 
+} #end function
